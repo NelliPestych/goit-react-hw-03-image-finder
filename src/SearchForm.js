e98ef36cab903991b848924c5771982f2766dbf4
NelliPestych <NelliPestych@bigmir.net>
2020-07-18 22:29:50 +0300

    homework

---
+++ src/SearchForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class SearchForm extends Component {
+    state = { inputValue: '' };
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.inputValue);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          enter search query
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    );
+  }
+}
---
+++ src/Spinner.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Spinner({ message }) {
+return <p>Whoops, something went erong: {message}</p>;
+}
